9e959b7176830131dbb4f59f847f66708180fdd8
Oussama <oussamafannah4@gmail.com>
2024-11-08 20:59:04 +0100

    commit 34

--- src/components/auth/register.tsx
+++ src/components/auth/register.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Alert from '../Alert';
 import "../../style/register.css";
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
 const RegisterForm = () => {
   const navigate = useNavigate();
