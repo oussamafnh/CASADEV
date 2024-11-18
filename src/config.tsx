968e4fb62e0d8045a91e1e8f8315dab695d95dbc
Oussama <oussamafannah4@gmail.com>
2024-11-18 18:30:13 +0100

    commit 69

--- src/config.tsx
+++ src/config.tsx
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = 'http://localhost:8090';
-export const API_BASE_URL = 'https://casadev2-5.onrender.com';
+export const API_BASE_URL = 'https://casadev2-4aiv.onrender.com';
