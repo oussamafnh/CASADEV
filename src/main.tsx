e5725e811614070244ea908a1cde74c1c0fb5d18
oussama <oussama.highness@gmail.com>
2025-01-08 00:49:08 +0100

    add stats x

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { injectContentsquareScript } from '@contentsquare/tag-sdk';
+
+injectContentsquareScript({
+  siteId: "5259375",
+  async: true,
+  defer: false,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
