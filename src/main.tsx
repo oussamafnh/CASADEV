2feb48df9652224d5df5d7757c6a562b1406578f
oussama <oussama.highness@gmail.com>
2025-01-08 01:24:21 +0100

    add stats microsoft

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { injectContentsquareScript } from '@contentsquare/tag-sdk';
-
-injectContentsquareScript({
-  siteId: "5259375",
-  async: true,
-  defer: false,
-});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
